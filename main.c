db30425c26cfabb2154f6be26c29f180f7abe402
ejuarezp <edjuarezp@itesm.mx>
2017-01-07 02:21:39 -0600

    dividir a entre b

---
+++ main.c
@@ -0,0 +1,22 @@
+/**
+ * Program to
+ * Author: Eduardo Juarez
+ * Date:
+ **/
+
+//Include libraries
+#include <stdlib.h> //Standard library
+#include <stdio.h> //Standard input / output
+
+/**
+ * Main function of the program
+ */
+int main() {
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    scanf("%i%*c", &a);
+    scanf("%i%*c", &b);
+    scanf("%i%*c", &c);
+    printf("%i", a / b);
+}
